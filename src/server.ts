0f40098a0cec98c6376539cac8fe53fb32cc7cfb
Shir Tunik <shirtunik@MacBook-Air-sl-Shir.local>
2026-01-08 18:19:36 +0200

    add body handle

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import postRouter from './routes/post.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(express.json());
 app.use("/post", postRouter);
 
 app.get('/', (req, res) => {
