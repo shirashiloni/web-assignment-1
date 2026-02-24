2f94c3fd3ae160ca6130216ae0ad7d3eb4540d99
shira <shira2003shira@gmail.com>
2026-02-24 19:49:06 +0200

    Fix comments count not updating

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -1,4 +1,5 @@
 import commentModel from "../model/comment.js";
+import postModel from "../model/post.js";
 import type { Request, Response } from "express";
 import BaseController from "./base.js";
 
@@ -8,19 +9,30 @@ class CommentController extends BaseController {
         super(commentModel);
     }
 
+
     async create(req: Request, res: Response) {
-        return super.create(req, res);
+        const result = await super.create(req, res);
+        try {
+            const postId = req.body.postId;
+            if (postId) {
+                await postModel.findByIdAndUpdate(postId, { $inc: { commentsCount: 1 } });
+            }
+        } catch (err) {
+            console.error('Failed to increment commentsCount:', err);
+        }
+        return result;
     }
 
     async del(req: Request, res: Response) {
         const id = req.params.id;
-        
         try {
             const comment = await this.model.findById(id);
-            
             if (!comment) {
                 res.status(404).send("Comment not found");
                 return;
+            }
+            if (comment.postId) {
+                await postModel.findByIdAndUpdate(comment.postId, { $inc: { commentsCount: -1 } });
             }
             super.del(req, res);
         } catch (err) {
@@ -37,7 +49,6 @@ class CommentController extends BaseController {
                 res.status(404).send("Comment not found");
                 return;
             }
-
             if (req.body.userId && req.body.userId !== comment.userId.toString()) {
                 res.status(403).send("Cannot change creator of the comment");
                 return;
